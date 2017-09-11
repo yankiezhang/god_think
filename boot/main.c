f7a6ebfd470717a4529dc6be1b7ef0addc5276d9
yang <yankiezhang@gmail.com>
2017-09-11 23:47:27 +0800

    load the god's secotr to the 1M

--- boot/main.c
+++ boot/main.c
@@ -3,10 +3,13 @@
 #include <print.h>
 #include <io.h>
 #include <sector.h>
+#include <reg.h>
 
 int kernel(void) 
 {
 	putchar('C');
+	set_es(WORD_SEL_GDT_GOD);
+//	set_ds(WORD_SEL_GDT_GOD);
 	readsect((void*)WORD_OFFSET_GOD, 1);
 	return ERR_NONE;
 }
